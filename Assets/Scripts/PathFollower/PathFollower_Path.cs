4ae76de87e7478df770079f5ef04b4c322227dbc
ariburacoLab <34109588+ariburaco@users.noreply.github.com>
2020-12-27 18:53:32 +0300

    Pid Control Class Added!

--- Assets/Scripts/PathFollower/PathFollower_Path.cs
+++ Assets/Scripts/PathFollower/PathFollower_Path.cs
@@ -8,87 +8,105 @@ using UnityEngine;
 public class PathFollower_Path : MonoBehaviour
 {
 
+
     public GameObject player;
     public GameObject wayPoint;
     public PathCreator pathCreation;
     public bool ActivateController = false;
     public bool ResetError = false;
-    public float Kp = 0.0001f;
-    public float Ki = 0.0001f;
-    public float Kd = 0.0001f;
-    public float Speed = 5f;
     public float ErrorThreshold = 0.1f;
-
-    private Vector3[] vertices;
-
-    float error = 0f;
-    float error_z1 = 0f;
-    float error_z2 = 0f;
-    float PWM = 0f;
-    float PWM_z1 = 0f;
+    public float Kp = 0.1f;
+    public float Ki = 0.1f;
+    public float Kd = 0.1f;
+    public float Speed = 10f;
 
     List<VertexDistance> vertexDistances = new List<VertexDistance>();
+    private Vector3[] vertices;
+    VertexDistance error_vector;
 
+    PID_Controller horizontalPID;
+    PID_Controller verticalPID;
 
     void Start()
     {
+        horizontalPID = new PID_Controller();
+        verticalPID = new PID_Controller();
         GetVertices();
-        CreateWayPoints();
-
+        //CreateWayPoints();
 
     }
 
     void LateUpdate()
     {
-       
+
     }
 
     void FixedUpdate()
     {
         if (ActivateController)
         {
-            VertexDistance error_vector = CalculateDistance();
 
-           // Debug.Log("x: " + error_vector.Direction.x + " z: " + error_vector.Direction.z);
 
-            error_z2 = error_z1;
-            error_z1 = error;
-            error = error_vector.Distance * error_vector.Direction;
-            PIDController(Kp, Ki, Kd);
-            MovePlayer(PWM);
+            error_vector = CalculateDistance();
+            float errorX = error_vector.Distance * error_vector.DirectionX;
+            float errorZ = error_vector.Distance * error_vector.DirectionZ;
+
 
             if (ResetError)
             {
-                error = 0;
-                error_z2 = 0;
-                error_z1 = 0;
-                PWM = 0;
-                PWM_z1 = 0;
+                errorX = 0;
+                errorZ = 0;
+                horizontalPID.ResetController();
             }
 
-            if (Math.Abs(error) < ErrorThreshold)
+            if (Math.Abs(errorX) < ErrorThreshold)
             {
-                PWM = 0f;
+                horizontalPID.ResetController();
             }
+
+            if (Math.Abs(errorZ) < ErrorThreshold)
+            {
+                verticalPID.ResetController();
+            }
+
+            horizontalPID.Kp = Kp;
+            horizontalPID.Ki = Ki;
+            horizontalPID.Kd = Kd;
+
+            verticalPID.Kp = Kp;
+            verticalPID.Ki = Ki;
+            verticalPID.Kd = Kd;
+
+            float x = horizontalPID.PID_Compute(errorX);
+            float z = verticalPID.PID_Compute(errorZ);
+
+            MovePlayer(x,z);
+
+
+
+
+            //Debug.Log("e: " + horizontalPID.Error + " PWM: " + horizontalPID.Output);
         }
 
 
 
-        Debug.Log("e: " + error + " PWM: " + PWM + " PWM_z1: " + PWM_z1);
+
 
     }
 
-    void MovePlayer(float x)
+    void MovePlayer(float x, float z)
     {
-        //float speed = 10;
+       
         float moveX = Speed * Input.GetAxis("Horizontal") * Time.deltaTime;
-        float moveZ = Speed  * Time.deltaTime; // Input.GetAxis("Vertical")
-        Vector3 translate = new Vector3(moveX, 0, moveZ);
-        player.transform.Translate(translate);
+        float moveZ = Speed * Input.GetAxis("Vertical") *  Time.deltaTime;
+        
+        
+        Vector3 translate;
+
 
         if (ActivateController)
         {
-            translate = new Vector3(x * Time.deltaTime, 0, 0);
+            translate = new Vector3(x * Time.deltaTime + moveX, 0, z * Time.deltaTime + moveZ);
             player.transform.Translate(translate);
         }
 
@@ -111,60 +129,71 @@ public class PathFollower_Path : MonoBehaviour
 
     VertexDistance CalculateDistance()
     {
-
         vertexDistances.Clear();
-        foreach (var point in vertices)
+
+        for (int i = 0; i < vertices.Length; i++)
         {
 
-            float distance = Vector3.Distance(point , player.transform.position);
+            Vector3 nextPoint = 
+            float distance = Vector3.Distance(point, player.transform.position);
             Vector3 direction = (point - player.transform.position).normalized;
 
             VertexDistance vertexDistance = new VertexDistance();
             vertexDistance.Point = point;
-            vertexDistance.Distance = distance;
+
             if (direction.x >= 0)
             {
-                vertexDistance.Direction = 1;
-
+                vertexDistance.DirectionX = 1;
             }
             else
             {
-                vertexDistance.Direction = -1;
+                vertexDistance.DirectionX = -1;
             }
 
+            if (direction.y >= 0)
+            {
+                vertexDistance.DirectionY = 1;
+            }
+            else
+            {
+                vertexDistance.DirectionY = -1;
+            }
+
+            if (direction.z >= 0)
+            {
+                vertexDistance.DirectionZ = 1;
+            }
+            else
+            {
+                vertexDistance.DirectionZ = -1;
+            }
+
+            vertexDistance.Distance = distance;
+
             vertexDistances.Add(vertexDistance);
+
+        }
+
+
+       
+        foreach (var point in vertices)
+        {
+
         }
 
         VertexDistance minDistancePoint = vertexDistances.Min();
         return minDistancePoint;
 
     }
-
-
-    void PIDController(float a, float b, float c)
-    {
-        float a0 = 1;
-        float a1 = a + b + c;
-        float a2 = (a * b + c * (b + a));
-        float a3 = a * b * c;
-
-        float Kd = -a3;
-        float Kp = 2 * Kd - a2;
-        float Ki = -(a1 + Kp + Kd);
-
-        PWM_z1 = PWM;
-        PWM = PWM_z1 + Kp * (error - error_z1) + Ki * error + Kd * (error - 2 * error_z1 + error_z2);
-       
-    }
-
-
 }
 
 public class VertexDistance : IComparable<VertexDistance>
 {
     public Vector3 Point { get; set; }
     public float Distance { get; set; }
-    public float Direction { get; set; }
+    public float DirectionX { get; set; }
+    public float DirectionY { get; set; }
+    public float DirectionZ { get; set; }
     int IComparable<VertexDistance>.CompareTo(VertexDistance other)
     {
         if (other.Distance > this.Distance)
@@ -177,19 +206,99 @@ public class VertexDistance : IComparable<VertexDistance>
 }
 
 
-/*
- function PWM = fcn(PWM_z1, error, error_z1, error_z2, a, b, c )
-
-% PWM = error * Kp;
-a0 = 1;
-a1 = a + b + c;
-a2 = (a*b + c*(b+a));
-a3 = a*b*c;
-
-Kd = -a3;
-Kp = 2*Kd - a2;
-Ki = -(a1 + Kp + Kd);
 
 
-PWM = PWM_z1 + Kp*(error - error_z1) + Ki * error + Kd * (error - 2*error_z1 + error_z2);
- */
+public class PID_Controller
+{
+
+    private float kp;
+
+    public float Kp
+    {
+        get { return kp; }
+        set { kp = value; }
+    }
+
+
+    private float ki;
+
+    public float Ki
+    {
+        get { return ki; }
+        set { ki = value; }
+    }
+
+    private float kd;
+
+    public float Kd
+    {
+        get { return kd; }
+        set { kd = value; }
+    }
+
+
+
+    private float error;
+
+    public float Error
+    {
+        get { return error; }
+        set { error = value; }
+    }
+
+
+    private float output;
+
+    public float Output
+    {
+        get { return output; }
+        set { output = value; }
+    }
+
+
+
+    private float output_z1 = 0f;
+    private float error_z1 = 0f;
+    private float error_z2 = 0f;
+
+
+    public PID_Controller()
+    {
+        error_z2 = 0;
+        error_z1 = 0;
+        error = 0;
+        output = 0;
+        output_z1 = 0;
+    }
+
+
+    /// <summary>
+    /// Compute the PID output each cycle.
+    /// </summary>
+    /// <returns>Returns the PID Output</returns>
+    public float PID_Compute(float _error)
+    {
+
+        error_z2 = error_z1;
+        error_z1 = error;
+        error = _error;
+
+        output = output_z1 + kp * (error - error_z1) + ki * error + kd * (error - 2 * error_z1 + error_z2);
+        output_z1 = output;
+
+       // Debug.Log(error + " " + error_z1 + " " + error_z2);
+
+        return output;
+    }
+
+    public void ResetController()
+    {
+        error = 0;
+        error_z1 = 0;
+        error_z2 = 0;
+        output = 0;
+        output_z1 = 0;
+        
+    }
+
+}
